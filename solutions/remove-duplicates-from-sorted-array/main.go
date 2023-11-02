b78dc1566d5e552364fb43423324e47449539c02
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2023-11-02 16:46:40 -0300

    passed! remove duplicates from sorted array

---
+++ solutions/remove-duplicates-from-sorted-array/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	k := 1 // unique elements
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}
+
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	k := removeDuplicates(nums)
+	fmt.Println(k, nums)
+}
